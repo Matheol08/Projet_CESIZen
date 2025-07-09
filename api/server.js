ac253266457bf645f4807a8c69da80350300dfec
Matheol08 <matheo.lesenne@viacesi.fr>
2025-07-09 11:20:21 +0200

    test deploiement

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME_prod,
   logging: false,
 });
 

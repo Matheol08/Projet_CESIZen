dc4ac710ef6e6385f46cb0d7bea5b4afaec04920
Matheol08 <matheo.lesenne@viacesi.fr>
2025-07-09 18:37:10 +0200

    test deploiement

--- api/controllers/menuController.js
+++ api/controllers/menuController.js
@@ -3,8 +3,10 @@ const Menu = require('../models/menu');
 exports.getMenus = async (req, res) => {
   try {
     const menus = await Menu.findAll(); 
-    res.json(menus);
-    console.log('Menus récupérés avec succès');
+    res.json({
+      data: menus,
+      message: 'Menus récupérés avec succès'
+    });
   } catch (error) {
     console.error('Erreur lors de la récupération des menus :', error);
     res.status(500).json({ message: 'Erreur serveur' });
